f388305d6e0939c0d40bb274dc5b06cb4b74a2d8
Fabian Eichelbrönner <fabian@surfgreen.dev>
2024-09-09 16:12:22 +0200

    start rules app and create rule model

---
+++ vue_js_django_prototype/rules/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Rule(models.Model):
+    protocol = models.CharField(max_length=50)
+    source = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    destination_port = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.protocol} - {self.source} -{self.destination} - {self.destination_port}'
